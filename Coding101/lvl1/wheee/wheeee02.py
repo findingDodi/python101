fccf9c3cdd7b4018735a83f37256f8160764a3c0
findingDodi <dorina.diem@gmail.com>
2023-02-18 21:27:39 +0100

    start of privateCalender + refactor for wheeee02.py

--- Coding101/lvl1/wheee/wheeee02.py
+++ Coding101/lvl1/wheee/wheeee02.py
@@ -1,11 +1,10 @@
 inputNumber = int(input("Enter MAX: "))
 
-
 if inputNumber >= 1:
     for i in range(inputNumber):
-        print("#" * (i+1))
+        print("#" * (i + 1))
         
-    for i in range(inputNumber-1, 0, -1):
+    for i in range(inputNumber - 1, 0, -1):
         print("#" * i)
 
 else:
---
+++ Coding101/lvl2/privateCalender/privateCalender01.py
@@ -0,0 +1,37 @@
+# Liste Tage des aktuellen Monats (eine Woche je Zeile, mit Wochennummer) Wochenanfang ist Montag
+import calendar
+import datetime
+
+
+def get_week_number(year, month, week):
+    week_reversed = week.copy()
+    week_reversed.sort(reverse=True)
+    week_number = datetime.date(year, month, week_reversed[0]).isocalendar().week
+    if week_number < 10:
+        return "0{} ".format(week_number)
+
+    return str(week_number)
+
+
+def print_month(year, month):
+    my_month = calendar.monthcalendar(year, month)
+
+    for week in my_month:
+        week_string = get_week_number(year, month, week)
+        for day in week:
+            if day == 0:
+                week_string += " ** "
+            elif day < 10:
+                week_string += " 0{} ".format(str(day))
+            else:
+                week_string += " {} ".format(str(day))
+        print(week_string)
+
+
+def print_header():
+    print(" #  {}  {}  {}  {}  {}  {}  {} ".format("Mo", "Tu", "We", "Th", "Fr", "Sa", "So"))
+
+
+if __name__ == "__main__":
+    print_header()
+    print_month(2023, 2)
