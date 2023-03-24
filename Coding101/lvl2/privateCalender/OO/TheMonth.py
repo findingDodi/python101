e16c757e034e968c34336ac8e54791c1864b7cf6
findingDodi <dorina.diem@gmail.com>
2023-03-24 15:41:30 +0100

    OO privateCalender

---
+++ Coding101/lvl2/privateCalender/OO/TheMonth.py
@@ -0,0 +1,6 @@
+
+class TheMonth:
+
+    def __init__(self):
+        month_number = 0
+        list_of_weeks = []
---
+++ Coding101/lvl2/privateCalender/OO/TheWeek.py
@@ -0,0 +1,6 @@
+
+class TheWeek:
+
+    def __init__(self):
+        week_number = 0
+        list_of_days = []
