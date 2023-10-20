14c1f21a826b86c42906617f2bbaf6bb4aff607a
findingDodi <dorina.diem@gmail.com>
2023-10-20 16:43:38 +0200

    optimal

--- advanced/fractality/fractality.py
+++ advanced/fractality/fractality.py
@@ -10,10 +10,10 @@ def convert(_initiator, _generator):
     return _initiator.replace("F", _generator)
 
 
-def move_and_turn(path, distance):
-    for current_move in path:
+def move_and_turn(_path, _distance):
+    for current_move in _path:
         if current_move == "F":
-            turtle.forward(distance)
+            turtle.forward(_distance)
         elif current_move == "-":
             turtle.left(60)
         elif current_move == "+":
@@ -23,12 +23,13 @@ def move_and_turn(path, distance):
 initiator = "F--F--F"
 generator = "F+F--F+F"
 loop_amount = 4
-distance = 20 / loop_amount
 turtle.shape("turtle")
 turtle.speed(0.1)
 
 for i in range(loop_amount):
+    distance = 20 / (i + 1)
     initiator = convert(initiator, generator)
     move_and_turn(initiator, distance)
+    # turtle.setheading(0)
 
 turtle.done()
